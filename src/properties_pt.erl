e8e1ee092b83e92dca49ea7e7d3b2a287c8f575c
Matthias Horn <matthiash@gmail.com>
2012-01-07 19:07:46 +0000

    unifies processing of sources
    matching functions
    value/value_code
    and static/load

--- src/properties_pt.erl
+++ src/properties_pt.erl
@@ -12,20 +12,53 @@
 parse_transform(Forms0,COptions) ->
   Forms1 = ?forms(Forms0),
   Module = getModule(Forms1),
-  Properties = getProperties(Forms1),
+  PropertyDefaults = getProperties(Forms1),
+  Properties = [P || {P,_} <- PropertyDefaults],
   Options = lists:flatten([ Opt ||  {?MODULE,Opt} <- proplists:get_all_values(?MODULE,COptions) ]),
-  Sources = getSources(Forms1),
+  Sources0 = static(getSources(Forms1)),
   GenCode = not(proplists:get_bool(no_codegen,Options)),
   OnLoad = not(proplists:get_bool(no_onload,Options)),
+  Static = proplists:get_bool(static,Options),
+  Sources = simplify(case Static of
+    true -> Sources0 ++ load(Sources0) ++ [{proplists,PropertyDefaults}];
+    _ -> Sources0 ++ [{proplists,PropertyDefaults}]
+  end,Properties), 
   ?debug(Module),
   ?debug(Properties),
+  ?debug(PropertyDefaults),
   ?debug(Options),
+  ?debug(Sources0),
   ?debug(Sources),
   ?debug(GenCode),
   ?debug(OnLoad),
-  Forms = code_gen(Module,Properties,Sources,Sources,GenCode,OnLoad),
+  ?debug(Static),
+  Forms = code_gen(Module,Properties,case Static of true -> load(Sources); _ -> Sources end,Sources,GenCode,OnLoad),
   ?debug("Forms = ~s",[erl_prettypr:format(?forms(Forms))]),
   Forms.
+
+simplify(Props,Keys) -> simplify(Props,[],Keys).
+simplify([],_Keys,_Only) -> [];
+simplify([{proplists,P1},{proplists,P2}|Ps],Keys,Only) ->
+  simplify([{proplists,P1++P2}|Ps],Keys,Only);
+simplify([{proplists,P1}|Ps],Keys1,Only) ->
+  case simplify2(P1,Keys1,Only,[]) of
+    {[],Keys2} -> simplify(Ps,Keys2,Only);
+    {P2,Keys2} -> [{proplists,P2}|simplify(P2,Keys2,Only)]
+  end;
+simplify([P|Ps],Keys,Only) -> [P|simplify(Ps,Keys,Only)].
+simplify2([P|Ps],Keys,Only,Accum) ->
+  {K,V} = lists:property(P),
+  case {is_in(K,Keys),is_in(K,Only)} of
+    {false,true} -> simplify2(Ps,[K|Keys],Only,[{K,V}|Accum]);
+    _ -> simplify2(Ps,Keys,Only,Accum)
+  end;
+simplify2([],Keys,_Only,Accum) -> {Accum,Keys}.
+
+is_in(_K,[]) -> false;
+is_in(K,[K|_]) -> true;
+is_in(K,[_|L]) -> is_in(K,L).
+  
+        
 
 reload(Module,Properties,Sources) ->
   Forms = code_gen(Module,Properties,load(Sources),Sources,true,false),
@@ -61,20 +94,81 @@ reload(Module,Properties,Sources) ->
 %   log_msgs(Module,E),
 %   log_msgs(Module,Es).
 
-load(Sources) ->
-  [ case Source of
-      {application,Application} -> {proplists,application:get_all_env(Application)};
-      {file,File} ->
-        case file:consult(File) of
-          {ok,Props} -> {proplists,Props};
-          {error,_} -> {proplists,[]}
-        end;
-      {proplists,Props} -> {proplists,Props};
-      _ -> {proplists,[]}
-    end || Source <- Sources ].
+static([]) -> [];
+static([A|B]) -> [static(A)|static(B)];
+static({static,A}) -> load(A);
+static(A) -> A.
+
+load([]) -> [];
+load([A|B]) -> [load(A)|load(B)];
+load({static,A}) -> load(A);
+load({application,A}) -> {proplists,application:get_all_env(A)};
+load({file,A}) -> 
+  case file:consult(A) of
+    {ok,Props} -> {proplists,Props};
+    {error,_} -> {proplists,[]}
+  end;
+load(A) -> A.
+
+-compile({nowarn_unused_function,[value/2]}).
+value(_Key,[]) -> undefined;
+value(Key,[A|B]) ->
+  case value(Key,A) of
+    undefined -> value(Key,B);
+    Val -> Val
+  end;
+value(Key,{static,A}) -> value(Key,A);
+value(Key,{application,A}) ->
+  case application:get_env(A,Key) of
+    {ok,Val} -> Val;
+    _ -> undefined
+  end;
+value(Key,{file,A}) ->
+  case file:consult(A) of
+    {ok,P} -> proplists:get_value(Key,P);
+    _ -> undefined
+  end;
+value(Key,{proplists,A}) ->
+  proplists:get_value(Key,A);
+value(_Key,_Unknown) -> undefined.
+
+value_code(Key,Source) ->
+  case value_code_(Key,Source) of
+    {Code,_Res} -> Code
+  end.
+
+value_code_(_Key,[]) -> {?atom(undefined),novalue};
+value_code_(Key,[A]) -> value_code(Key,A);
+value_code_(Key,[A|B]) ->
+  case value_code(Key,A) of
+    {Code,value} -> {Code,value};
+    {ACode,ARes} -> 
+      case value_code_(Key,B) of
+        {_,novalue} -> {ACode,ARes};
+        {BCode,BRes} ->
+          Var = var(),
+          {?cases(ACode,[?clause([?atom(undefined)],none,[BCode]),?clause([Var],none,[Var])]),BRes}
+      end
+  end;
+value_code_(Key,{static,A}) -> value_code_(Key,load(A));
+value_code_(Key,{application,A}) ->
+  {?apply(application,get_env,[?atom(A),?atom(Key)]),undefined};
+value_code_(Key,{file,A}) ->
+  Var = var(),
+  {?cases(?apply(file,consult,[?string(A)]),
+    [ ?clause([?tuple([?atom(ok),Var])],none,[?apply(proplists,get_value,[?atom(Key),Var])])
+    , ?clause([?underscore],none,[?atom(undefined)])
+    ]),undefined};
+value_code_(Key,{proplists,A}) ->
+  case proplists:get_value(Key,A) of
+    undefined -> {?atom(undefined),novalue};
+    V -> {?abstract(V),value}
+  end;
+value_code_(_Key,_Unknown) ->
+  {?atom(undefined),novalue}.
   
-  
-  
+
+
 code_gen(Module,Properties,Sources,OrigSources,GenCode,OnLoad) ->
   % io:format("~p~n",[catch erlang:error(debug,[Module,Properties,Sources,GenCode,OnLoad])]),
   NeedsGen = lists:any(fun ({application,_}) -> true; ({file,_}) -> true; (_) -> false end,OrigSources),
@@ -109,7 +203,7 @@ code_gen(Module,Properties,Sources,OrigSources,GenCode,OnLoad) ->
   end
   ++
   [ 
-      gen_prop(Prop,Sources++[{proplists,Properties}])
+      gen_prop(Prop,Sources)
   || {Prop,_} <- Properties ]
   ++
   case NeedsGen andalso GenCode of
@@ -137,29 +231,7 @@ code_gen(Module,Properties,Sources,OrigSources,GenCode,OnLoad) ->
 
 
 gen_prop(Prop,Sources) -> 
-  ?function(Prop,[?clause([],none,[gen_prop_value(Prop,Sources)])]).
-gen_prop_value(_Prop,[]) -> ?atom(undefined);
-gen_prop_value(Prop,[{proplists,PropList}|Sources]) ->
-  case proplists:lookup(Prop,PropList) of
-    {Prop,Val} -> ?abstract(Val);
-    none -> gen_prop_value(Prop,Sources)
-  end;
-gen_prop_value(Prop,[{application,Application}|Sources]) ->
-  ?cases(?apply(application,get_env,[?atom(Application),?atom(Prop)]),
-    [ ?clause([?tuple([?atom(ok),?var('Value')])],none,[?var('Value')])
-    , ?clause([?atom(undefined)],none,[gen_prop_value(Prop,Sources)])
-    ]);
-gen_prop_value(Prop,[{file,File}|Sources]) ->
-  Var = ?var("Prop"++integer_to_list(length(Sources))),
-  ?cases(
-    ?cases(?apply(file,consult,[?string(File)]),
-      [ ?clause([?tuple([?atom(ok),Var])],none,[?apply(proplists,lookup,[?atom(Prop),Var])])
-      , ?clause([?tuple([?atom(error),?underscore])],none,[?atom(none)])
-      ]),
-    [ ?clause([?tuple([?atom(Prop),?var('Value')])],none,[?var('Value')])
-    , ?clause([?atom('none')],none,[gen_prop_value(Prop,Sources)])
-    ]);
-gen_prop_value(Prop,[_|Sources]) -> gen_prop_value(Prop,Sources).
+  ?function(Prop,[?clause([],none,[value_code(Prop,Sources)])]).
 
 gen_reload(Module,Properties,Sources) ->
   ?function(reload,
@@ -223,7 +295,7 @@ getSources(Forms) ->
         case erl_syntax:type(Form) of
           attribute ->
             case erl_syntax_lib:analyze_attribute(Form) of
-              {source,{source,Source}} -> Accum ++ [ Source ];
+              {source,{source,Source}} -> Accum ++ lists:flatten([Source]);
               _ -> Accum
             end;
           _ -> Accum
